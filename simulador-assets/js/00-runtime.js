f1d173b44e9dcfc9895c357895cb0dae5b86ab5b
Edgar <edgarchavez939@gmail.com>
2026-09-14 15:28:20 -0500

    Add files via upload

--- simulador-assets/js/00-runtime.js
+++ simulador-assets/js/00-runtime.js
@@ -23,7 +23,7 @@
   Object.defineProperty(global, 'PROGRAMAS', {value: Object.freeze(readEmbeddedData('programas-data')), writable:false, configurable:false});
   Object.defineProperty(global, 'POSGRADOS', {value: Object.freeze(readEmbeddedData('posgrados-data')), writable:false, configurable:false});
 
-  app.version = 'UX-P2.0';
+  app.version = 'UX-P2.1-visual';
   app.config = Object.freeze({
     garantisaCP: 0.0417,
     garantisaLP: 0.0286,
